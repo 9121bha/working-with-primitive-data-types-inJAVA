46cacd75995aff69049f3bbf007ee74b1043b2f5
9121bha <150103887+9121bha@users.noreply.github.com>
2024-09-02 19:38:59 +0530

    first commit

---
+++ src/customer.java
@@ -0,0 +1,65 @@
+public class customer {
+
+
+    public static void main(String[] args) {
+        /*working with byte data type
+
+         */
+        byte age;
+        age = 35;
+        System.out.println("age:" + age);
+
+
+
+
+        /*working with short data type
+
+         */
+        short value;
+        value = 5434;
+        System.out.println("order value:" + value);
+
+        /*working with int data type
+
+         */
+        int salary;
+        salary = 5444463;
+        System.out.println("salary :" + salary);
+        /*
+        working with long data type
+         */
+        long totalSavings;
+        totalSavings = 5674564564l;
+        System.out.println("totalSavings:" + totalSavings);
+
+        /*working with float data type/*
+
+         */
+        float avg;
+        avg = 232323.55f;
+        System.out.println("avg salary :"+avg);
+
+        /*
+        working with double data type
+         */
+
+        double avgrevenue;
+        avgrevenue = 23232323.34;
+        System.out.println("avgrevenue:"+ 2.323232334E7);
+
+        /*
+        working with char data type
+         */
+
+        char gender;
+        gender='m';
+        System.out.println("gender :"+ gender);
+
+     /*
+     working with bolean
+      */
+        boolean iseligible;
+        iseligible=true;
+        System.out.println("iseligible :"+ iseligible);
+    }
+}
---
+++ src/loops/java/loops.java
@@ -0,0 +1,7 @@
+package loops.java;
+
+public class loops {
+    public static void main(String[] args) {
+        System.out.println("he");
+    }
+}
